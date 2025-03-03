c8ef8ba87a30b5f21e0082ff3edc0bb7a20c1616
Daniel <dani.damqnov.dimitrov@gmail.com>
2025-03-03 15:44:00 +0200

    Added timeout and rate limiting policies

--- Servers/Yarp Gateway/src/MyTeamsHub.Yarp.Gateway.API/Program.cs
+++ Servers/Yarp Gateway/src/MyTeamsHub.Yarp.Gateway.API/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = 429;
+    options.AddFixedWindowLimiter("default-rate-limiter-policy", opt =>
+    {
+        opt.PermitLimit = 4;
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
+});
+
+builder.Services.AddRequestTimeouts(options =>
+{
+    options.AddPolicy("default-timeout-policy", TimeSpan.FromSeconds(20));
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
+
+app.UseRequestTimeouts();
 
 app.MapReverseProxy();
 
